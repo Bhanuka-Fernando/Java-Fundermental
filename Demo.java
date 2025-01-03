73f4f059e1d2a16826fb86a53da55b595c4e1e60
Bhanuka-Fernando <bhanukafernando2@gmail.com>
2025-01-03 07:39:51 +0530

    created Demo class

---
+++ Demo.java
@@ -0,0 +1,22 @@
+class Demo
+{
+    public static void main(String args[])
+    // main is the staring point of execution
+    {
+        //run the code according to the order
+        
+        show();  //  below created method is called
+
+        int num = 5;
+        double d = 9.5;
+        float f = 8.5f;
+        char name = 'A';
+
+        System.out.println(name + " " + num); // for concatinate keep space using inverted commas
+    }
+    
+    public static void show()
+    {
+        System.out.println("Show off");
+    }
+}
